90db5d9dab7a86a36ad3fc4ca4c79b1fbd976b75
evan_herren <evan@herren.co.kr>
2021-12-04 23:55:56 +0900

    1204115555

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+from .models import *
 
 # Create your views here.
 def index(request):
     #return HttpResponse("index")
-    return render(request, 'shareRes/index.html')
+    categories = Category.objects.all()
+    content = {'categories': categories}
+    return render(request, 'shareRes/index.html', content)
 
 
 def restaurantDetail(request):
@@ -17,6 +22,20 @@ def restaurantCreate(request):
 
 def categoryCreate(request):
     #return HttpResponse("categoryCreate")
-    return render(request, 'shareRes/categoryCreate.html')
+    categories = Category.objects.all()
+    content = {'categories':categories}
+    return render(request, 'shareRes/categoryCreate.html', content)
 
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def Delete_category(request):
+    category_id = request.POST['categoryId']
+    delete_category = Category.objects.get(id = category_id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse('cateCreatePage'))
+    #return HttpResponse("여기서 카테고리 생성 기능이 구현 됩니다.")
 
