74582fe29e8c4a502125dbaa1d6027907f256001
evan_herren <evan@herren.co.kr>
2021-12-05 21:08:44 +0900

    CRUD

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def sendEmail(request):
-    return HttpResponse("sendEmail")
+    return HttpResponseRedirect(reverse('index'))
+    #return HttpResponse("sendEmail")
